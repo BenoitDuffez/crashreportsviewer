0fb99937809503ee9166e9ee8a04f70036025e6c
BicouQ <benoit.duffez@gmail.com>
2013-03-15 12:10:45 +0100

    This should fix issue #2

--- www/html.php
+++ www/html.php
@@ -2,37 +2,18 @@
 
 function show_output() {
 	$html = ob_get_clean();
-	
-?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" >
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Crash Reports</title>
-		<script type="text/javascript" language="javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-		<script type="text/javascript" language="javascript">
-	function setStatusAndGo(iid, stat, url) {
-		$.get("/ajax.php", { action: "update_status", status: stat, issue_id: iid }, function() {
-			document.location=url;
-		});
+	$template = file_get_contents($_SERVER[DOCUMENT_ROOT]."./template.html");
+
+	$regex="#^(.*)/".$_GET[package]."#";
+	if ($_GET[package] && preg_match($regex, $_SERVER[REQUEST_URI], $matches)) {
+		$root = "/".$matches[1];
+	} else {
+		$root = "";
 	}
-		</script>
-		<!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
-		<script language="javascript" type="text/javascript" src="/dist/jquery.jqplot.min.js"></script>
-		<script type="text/javascript" src="/dist/plugins/jqplot.pieRenderer.min.js"></script>
-		<script type="text/javascript" src="/dist/plugins/jqplot.dateAxisRenderer.min.js"></script>
-		<script type="text/javascript" src="/dist/plugins/jqplot.highlighter.min.js"></script>
-		<script type="text/javascript" src="/dist/plugins/jqplot.cursor.min.js"></script>
 
-		<link rel="stylesheet" type="text/css" href="/dist/jquery.jqplot.css" />
-		<link rel="stylesheet" type="text/css" href="/style.css"></link>
-	</head>
-
-	<body>
-	<h1 style="text-align: center;"><a href="reports.php">Crash Reports</a></h1>
-	<?php echo $html; ?>
-	</body>
-</html>
-<?php
+	$patt = array("%ROOT%", "%HTML");
+	$repl = array($root, $html);
+	echo str_replace($patt, $repl, $template);
 }
 
 ob_start();
