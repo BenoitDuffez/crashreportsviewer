96dd6de066a896bab4546b51dd2af99463c024a5
BicouQ <benoit.duffez@gmail.com>
2013-03-15 12:17:11 +0100

    slight fixes again :)

--- www/html.php
+++ www/html.php
@@ -4,14 +4,16 @@ function show_output() {
 	$html = ob_get_clean();
 	$template = file_get_contents($_SERVER[DOCUMENT_ROOT]."./template.html");
 
-	$regex="#^(.*)/".$_GET[package]."#";
+	$regex = "#^(.*)/".$_GET[package]."#";
 	if ($_GET[package] && preg_match($regex, $_SERVER[REQUEST_URI], $matches)) {
-		$root = "/".$matches[1];
+		$root = $matches[1];
+	} else if (preg_match("#(.*)/([^\.]+)\.php#", $_SERVER[REQUEST_URI], $matches)) {
+		$root = $matches[1];
 	} else {
-		$root = "";
+		$root = "/";
 	}
 
-	$patt = array("%ROOT%", "%HTML");
+	$patt = array("%ROOT%", "%HTML%");
 	$repl = array($root, $html);
 	echo str_replace($patt, $repl, $template);
 }
